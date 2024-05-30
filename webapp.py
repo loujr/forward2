656b95d5be57c831c042715cd4caf9fcef9db059
loujr <loujr@github.com>
2024-05-30 15:00:45 +0000

    commented out core functions of webapp.py to export to website.py

--- webapp.py
+++ webapp.py
@@ -19,43 +19,43 @@ app = Flask(__name__, subdomain_matching=True)
 #app.config['APIENDPOINT'] = os.getenv("APIENDPOINT")
 
 # Initialize the cache
-cache = Cache()
+#cache = Cache()
 # Initialize the API
-api = Api(app)
+#api = Api(app)
 # Dictionary to store the shortened URLs
-shortened_urls = {}
+#shortened_urls = {}
 
 # Create a table in the database to store the shortened URLs
-def create_table():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    try:
-        cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='urls' ''')
-
-        # If the count is 1, then table exists
-        if cursor.fetchone()[0] == 1:
-            print('Table already exists.')
-        else:
-            conn.execute('CREATE TABLE urls (short_url TEXT, long_url TEXT)')
-            print('Table created successfully.')
-    except sqlite3.Error as e:
-        print(f"An error occurred: {e.args[0]}")
-    finally:
-        conn.close()
+#def create_table():
+#    conn = get_db_connection()
+#    cursor = conn.cursor()
+#    try:
+#        cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='urls' ''')
+#
+#        # If the count is 1, then table exists
+#        if cursor.fetchone()[0] == 1:
+#            print('Table already exists.')
+#        else:
+#            conn.execute('CREATE TABLE urls (short_url TEXT, long_url TEXT)')
+#            print('Table created successfully.')
+#    except sqlite3.Error as e:
+#        print(f"An error occurred: {e.args[0]}")
+#    finally:
+#        conn.close()
 
 # Get a connection to the database
-def get_db_connection():
-    conn = sqlite3.connect('urls.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+#def get_db_connection():
+#    conn = sqlite3.connect('urls.db')
+#    conn.row_factory = sqlite3.Row
+#    return conn
 
 # Generate a short URL
-def generate_short_url(length=6):
-    chars = string.ascii_letters + string.digits 
-    short_url = "".join(random.choice(chars) for _ in range(length))
-    return short_url
+#def generate_short_url(length=6):
+#    chars = string.ascii_letters + string.digits 
+#    short_url = "".join(random.choice(chars) for _ in range(length))
+#    return short_url
 
-# Route to the home page
+#Route to shorten a URL
 @app.route('/shorten_url', methods=['POST'])
 def shorten_url():
     data = request.get_json()
@@ -72,15 +72,15 @@ def shorten_url():
 #{"short_url":"https://localhost/3lupAP"}
 
 # Route to redirect the short URL to the original URL
-@cache.cached(timeout=60)
-@app.route("/<short_url>")
-def redirect_url(short_url):
-    conn = get_db_connection()
-    url_data = conn.execute('SELECT long_url FROM urls WHERE short_url = ?', (short_url,)).fetchone()
-    if url_data is None:
-        return "URL not found", 404
-    else:
-        return redirect(url_data['long_url'], code=302)
+#@cache.cached(timeout=60)
+#@app.route("/<short_url>")
+#def redirect_url(short_url):
+#    conn = get_db_connection()
+#    url_data = conn.execute('SELECT long_url FROM urls WHERE short_url = ?', (short_url,)).fetchone()
+#    if url_data is None:
+#        return "URL not found", 404
+#    else:
+#        return redirect(url_data['long_url'], code=302)
 
 # Accepted methods for additional API endpoints
 APIENDPOINT = ['GET', 'POST']
--- website/website.py
+++ website/website.py
@@ -8,18 +8,62 @@ import string
 import sqlite3
 from flask_restful import Api
 
+# Load environment variables from a .env file
 load_dotenv()
 
+# Initialize Flask app with subdomain matching enabled
 app = Flask(__name__, subdomain_matching=True)
+
+# Initialize the cache
 cache = Cache()
+# Initialize the API
 api = Api(app)
+# Dictionary to store the shortened URLs
 shortened_urls = {}
 
+#Get a connection to the database
+def get_db_connection():
+    conn = sqlite3.connect('urls.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+# Create a table in the database to store the shortened URLs
+def create_table():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='urls' ''')
+
+        # If the count is 1, then table exists
+        if cursor.fetchone()[0] == 1:
+            print('Table already exists.')
+        else:
+            conn.execute('CREATE TABLE urls (short_url TEXT, long_url TEXT)')
+            print('Table created successfully.')
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e.args[0]}")
+    finally:
+        conn.close()
 
 
+# Generate a short URL
+def generate_short_url(length=6):
+    chars = string.ascii_letters + string.digits 
+    short_url = "".join(random.choice(chars) for _ in range(length))
+    return short_url
 
 
-
+# Route to redirect the short URL to the original URL
+@cache.cached(timeout=60)
+@app.route("/<short_url>")
+def redirect_url(short_url):
+    conn = get_db_connection()
+    url_data = conn.execute('SELECT long_url FROM urls WHERE short_url = ?', (short_url,)).fetchone()
+    if url_data is None:
+        return "URL not found", 404
+    else:
+        return redirect(url_data['long_url'], code=302)
+    
 
 if __name__ == "__main__":
     app.run()
